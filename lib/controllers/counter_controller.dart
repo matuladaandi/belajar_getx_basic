f8b074487b43de93d24a194de33d4f472fae5859
andi <echo.matuladaandi@gmail.com>
2023-11-21 16:19:07 +0800

    add coment di file controller terkait penjelasan obs

--- lib/controllers/counter_controller.dart
+++ lib/controllers/counter_controller.dart
@@ -1,7 +1,7 @@
 import 'package:get/state_manager.dart';
 
 class CounterController extends GetxController {
-  var counter = 0.obs; // tipedata Nya int
+  var counter = 0.obs; // tipedata Nya int, obs =  obeservasi artinya akan selalu dipantau
   var isDark = false.obs;
 
   void changeTheme() => isDark.value = !isDark.value;
