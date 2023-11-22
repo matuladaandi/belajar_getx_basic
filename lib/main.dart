d5d911473e32f52110181128d0731a5ef7c64f47
andi <echo.matuladaandi@gmail.com>
2023-11-23 07:26:08 +0800

    hal yang bisa dilakukan statefull

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:getx/controllers/counter_controller.dart';
 
 void main() {
   // ignore: prefer_const_constructors
@@ -11,56 +9,107 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  final counterCtrl = Get.put(CounterController());
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Learning Getx'),
-          centerTitle: true,
-        ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // simple GetBuilder()
-            GetBuilder<CounterController>(
-              id: "12",  // id bertipe object, Hnaya ada di GetBuilder()
-              builder: (_) {
-                return Text(
-                  'Angka ${counterCtrl.counter}',
-                  style: const TextStyle(fontSize: 35),
-                );
-              },
-            ),
-            GetBuilder<CounterController>(
-              id: "13",
-              builder: (_) {
-                return Text(
-                  'Angka ${counterCtrl.counter}',
-                  style: const TextStyle(fontSize: 35),
-                );
-              },
-            ),
-            GetBuilder<CounterController>(
-              id: "14",
-              builder: (_) {
-                return Text(
-                  'Angka ${counterCtrl.counter}',
-                  style: const TextStyle(fontSize: 35),
-                );
-              },
-            ),
-          ],
-        )),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            counterCtrl.increment();
-          },
-        ),
+      home: HomePage(),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Learning Getx Life Cycle'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () => Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => const OtherPage(),
+                    ),
+                  ),
+              icon: const Icon(Icons.refresh))
+        ],
+      ),
+      body: Center(
+        child: CountWidget(count: count),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class CountWidget extends StatefulWidget {
+  final int count;
+  CountWidget({super.key, required this.count});
+
+  @override
+  State<CountWidget> createState() => _CountWidgetState();
+}
+
+class _CountWidgetState extends State<CountWidget> {
+  @override
+  void didChangeDependencies() {
+    print('didChangeDependencies');
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(covariant CountWidget oldWidget) {
+    print('old  widget :  $oldWidget -- didUpdateWidget');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    // untuk clearkan
+    print('dispose');
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    print('initState');
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Text(
+      'Angka ${widget.count}',
+      style: const TextStyle(fontSize: 35),
+    );
+  }
+}
+
+class OtherPage extends StatelessWidget {
+  const OtherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Other Page'),
+        centerTitle: true,
       ),
     );
   }
